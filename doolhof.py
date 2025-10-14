401b680582ac0b679cb4865c72108ae8c56c020b
Renaud Leroy <remastered.king@gmail.com>
2025-10-14 10:05:00 +0200

    dfs 1

--- doolhof.py
+++ doolhof.py
@@ -1,3 +1,17 @@
+def DFS(rij,kolom):
+    
+
+    check of rij/kolom in doolhof ?
+
+    check of rij/kolom B ?
+
+    check of rij/kolom # ?
+
+    links DFS
+    boven DFS
+    rechts DFS
+    onder DFS
+
 def bekijkCel(rij,kolom):
     print(doolhof[rij][kolom])
 
@@ -15,11 +29,19 @@ def toonDoolhof():
 def maakDoolhof(rijen,kolommen):
     return [[' ']*rijen for kolom in range(kolommen)]
 
-doolhof = maakDoolhof(20,20)
+max_rij = 20
+max_kolom = 20
+
+doolhof = maakDoolhof(max_rij,max_kolom)
+
+doolhof[0][0]="A"
+
+doolhof[4][5]="B"
 
 maakMuur(1,1)
 maakMuur(2,4)
-toonDoolhof()
 maakMuur(3,6)
 toonDoolhof()
+
+DFS(0,0)
 
