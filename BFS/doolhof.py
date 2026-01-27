7bbe0769279a335478f4d20563007974bcdd135f
Renaud Leroy <remastered.king@gmail.com>
2026-01-27 10:25:14 +0100

    bfs loop

--- BFS/doolhof.py
+++ BFS/doolhof.py
@@ -6,13 +6,25 @@ def BFS():
     while len(open) > 0:
         #pop selecteert en verwijderd een element op een bepaalde plaats
         huidige = open.pop(0)
-
-        (3,5)
-
         # [] bij 'coördinaat' is het selecteert van een waarde (0=eerste, 1=tweede)
         # bijvoorbeeld bij cor = (3,5) is cor[0] = 3 en is cor[1] = 5
         x = huidige[0]
         y = huidige[1]
+
+        links = (x-1,y)
+        boven = (x,y-1)
+        rechts = (x+1,y)
+        onder = (x,y+1)
+
+        richtingen = [links,boven,rechts,onder]
+        for richting in richtingen:
+            r_x = richting[0]
+            r_y = richting[1]
+            
+            if r_x < 0:
+                #continue betekent naar de volgende richting in de herhaling gaan
+                continue
+
 
 def maakMuur(rij,kolom):
     doolhof[rij][kolom] = "#"
